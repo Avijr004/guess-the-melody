0745a743bd89c54e8f2ec93fd7860b1060bb5692
Avijr004 <128925724+Avijr004@users.noreply.github.com>
2023-11-28 18:23:58 +0530

    Update config.py

--- quiz/config.py
+++ quiz/config.py
@@ -1,6 +1,6 @@
 class Config:
     """Bot setup config implementation."""
 
-    token: str = ""  # should be API token provided by @botfather
+    token: str = "6491531689:AAFaxNvZyiyPE8lhZlo2dNv45yZoHVjSSIQ"  # should be API token provided by @botfather
     db_name: str = "melody.db"
     shelve_name: str = "shelve"
